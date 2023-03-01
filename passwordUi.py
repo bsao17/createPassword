5634fd5aec9760d6b11f029cf8146e0e4e5c864f
bruno <declic62@gmail.com>
2023-03-01 11:25:34 +0100

    fix: update tests

--- passwordUi.py
+++ passwordUi.py
@@ -5,9 +5,10 @@ class App(QtWidgets.QWidget):
     def __int__(self):
         super.__init__()
 
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    win = App()
+    win.show()
+    app.exec_()
 
-app = QtWidgets.QApplication([])
-win = App()
-win.show()
 
-app.exec_()
